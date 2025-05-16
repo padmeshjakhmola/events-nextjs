77477034231d482547b3a57c2b2d82a8f42489e4
Padmesh Jakhmola <prankshadow@gmail.com>
2025-05-16 20:30:53 +0530

    feat: added events

--- app/(root)/events/page.tsx
+++ app/(root)/events/page.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Image from "next/image";
 
 const Events = () => {
   return (
-    <div>Events</div>
-  )
-}
+    <div className="py-32 px-28">
+      <div className="flex flex-row justify-between items-center">
+        <h1 className="text-4xl font-semibold select-none">My Events</h1>
+        <div className="space-x-4">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                size="lg"
+                className="text-lg py-6 text-center cursor-pointer select-none bg-[#334155] hover:bg-gray-600"
+              >
+                Sort By
+                <Image
+                  src="/assets/icons/Vector.svg"
+                  width={10}
+                  height={10}
+                  alt="icon"
+                  className="object-contain"
+                />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>Accending</DropdownMenuItem>
+              <DropdownMenuItem>Decending</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <Button size="lg" className="text-lg py-6 text-center cursor-pointer">
+            + Add Event
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Events
+export default Events;
