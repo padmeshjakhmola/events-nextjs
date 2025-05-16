1222cf6b19767e592da3d8861bd424bb1a65b023
Padmesh Jakhmola <prankshadow@gmail.com>
2025-05-16 21:49:10 +0530

    not found

---
+++ app/not-found.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+
+    <Image
+      src="/assets/icons/404error.svg"
+      alt="error"
+      width={600}
+      height={600}
+      />
+      </div>
+  );
+};
+
+export default Page;
